374f5390f4d056ff8eaa9492d7ba188c8a23b814
Ankitak-ai <a92617177@gmail.com>
2026-03-10 12:54:42 +0530

    Create StarlightAnyaObsAlerts.tsx

---
+++ src/pages/obs-alerts/StarlightAnyaObsAlerts.tsx
@@ -0,0 +1,7 @@
+import { ObsAlertsWrapper } from '@/components/obs/ObsAlertsWrapper';
+
+const StarlightAnyaObsAlerts = () => (
+  <ObsAlertsWrapper streamerSlug="starlight_anya" storagePrefix="starlight_anya" />
+);
+
+export default StarlightAnyaObsAlerts;
