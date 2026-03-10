2d2867d54f068f5daa7a80e761fc9826871a511b
Ankitak-ai <a92617177@gmail.com>
2026-03-10 20:53:53 +0530

    Create ReynaYadavGoalOverlay.tsx

---
+++ src/pages/obs-alerts/ReynaYadavGoalOverlay.tsx
@@ -0,0 +1,9 @@
+import { GoalOverlayWrapper } from '@/components/obs/GoalOverlayWrapper';
+
+const ReynaYadavGoalOverlay = () => (
+  <div className="w-screen h-screen bg-transparent overflow-hidden flex items-center justify-center">
+    <GoalOverlayWrapper streamerSlug="reyna_yadav" />
+  </div>
+);
+
+export default ReynaYadavGoalOverlay;
