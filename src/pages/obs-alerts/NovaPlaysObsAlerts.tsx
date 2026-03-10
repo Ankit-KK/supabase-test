8d1a57cf046f5aa05417615e637bb754d67886ce
Ankitak-ai <a92617177@gmail.com>
2026-03-10 09:25:25 +0530

    Create NovaPlaysObsAlerts.tsx

---
+++ src/pages/obs-alerts/NovaPlaysObsAlerts.tsx
@@ -0,0 +1,7 @@
+import { ObsAlertsWrapper } from '@/components/obs/ObsAlertsWrapper';
+
+const NovaPlaysObsAlerts = () => (
+  <ObsAlertsWrapper streamerSlug="nova_plays" storagePrefix="nova_plays" />
+);
+
+export default NovaPlaysObsAlerts;
