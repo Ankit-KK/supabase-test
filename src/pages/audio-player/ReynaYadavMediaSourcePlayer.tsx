7e04a37e20148e904152a1db0befb11c9ea850eb
Ankitak-ai <a92617177@gmail.com>
2026-03-10 20:54:36 +0530

    Create ReynaYadavMediaSourcePlayer.tsx

---
+++ src/pages/audio-player/ReynaYadavMediaSourcePlayer.tsx
@@ -0,0 +1,13 @@
+import MediaSourcePlayer from '@/components/audio-player/MediaSourcePlayer';
+
+const ReynaYadavMediaSourcePlayer = () => (
+  <MediaSourcePlayer
+    streamerSlug="reyna_yadav"
+    streamerName="Reyna Yadav"
+    tableName="reyna_yadav_donations"
+    browserSourcePath="/reyna_yadav/media-audio-player"
+    brandColor="#f43f5e"
+  />
+);
+
+export default ReynaYadavMediaSourcePlayer;
