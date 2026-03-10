b0377c3666adc1725318ef424ab9ca986acb2aeb
Ankitak-ai <a92617177@gmail.com>
2026-03-10 20:53:22 +0530

    Create ReynaYadavObsAlerts.tsx

---
+++ src/pages/obs-alerts/ReynaYadavObsAlerts.tsx
@@ -0,0 +1,7 @@
+import { ObsAlertsWrapper } from '@/components/obs/ObsAlertsWrapper';
+
+const ReynaYadavObsAlerts = () => (
+  <ObsAlertsWrapper streamerSlug="reyna_yadav" storagePrefix="reyna_yadav" />
+);
+
+export default ReynaYadavObsAlerts;
