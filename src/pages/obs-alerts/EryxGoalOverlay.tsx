1b2f67ce0de5050062d0e19e899c2850398c95cd
Ankit Kumar <ankitashuk20@gmail.com>
2026-03-16 02:47:27 +0530

    Create EryxGoalOverlay.tsx

---
+++ src/pages/obs-alerts/EryxGoalOverlay.tsx
@@ -0,0 +1,9 @@
+import { GoalOverlayWrapper } from '@/components/obs/GoalOverlayWrapper';
+
+const EryxGoalOverlay = () => (
+  <div className="w-screen h-screen bg-transparent overflow-hidden flex items-center justify-center">
+    <GoalOverlayWrapper streamerSlug="eryx" />
+  </div>
+);
+
+export default EryxGoalOverlay;
