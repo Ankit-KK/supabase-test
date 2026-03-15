0359b88edd23e229228a5b1e4f1321dd4dfddc7b
Ankit Kumar <ankitashuk20@gmail.com>
2026-03-16 02:46:37 +0530

    Create EryxObsAlerts.tsx

---
+++ src/pages/obs-alerts/EryxObsAlerts.tsx
@@ -0,0 +1,7 @@
+import { ObsAlertsWrapper } from '@/components/obs/ObsAlertsWrapper';
+
+const EryxObsAlerts = () => (
+  <ObsAlertsWrapper streamerSlug="eryx" storagePrefix="eryx" />
+);
+
+export default EryxObsAlerts;
