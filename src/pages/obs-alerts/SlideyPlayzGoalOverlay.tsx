2e5fda3f023d58c889511e6e10ba317b8e98e440
Ankit Kumar <ankitashuk20@gmail.com>
2026-03-16 02:47:06 +0530

    Create SlideyPlayzGoalOverlay.tsx

---
+++ src/pages/obs-alerts/SlideyPlayzGoalOverlay.tsx
@@ -0,0 +1,9 @@
+import { GoalOverlayWrapper } from '@/components/obs/GoalOverlayWrapper';
+
+const SlideyPlayzGoalOverlay = () => (
+  <div className="w-screen h-screen bg-transparent overflow-hidden flex items-center justify-center">
+    <GoalOverlayWrapper streamerSlug="slidey_playz" />
+  </div>
+);
+
+export default SlideyPlayzGoalOverlay;
