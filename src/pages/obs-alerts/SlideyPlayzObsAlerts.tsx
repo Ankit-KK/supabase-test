7fc6460f29751f461d40990da13fea02addb2bbf
Ankit Kumar <ankitashuk20@gmail.com>
2026-03-16 02:46:17 +0530

    Create SlideyPlayzObsAlerts.tsx

---
+++ src/pages/obs-alerts/SlideyPlayzObsAlerts.tsx
@@ -0,0 +1,7 @@
+import { ObsAlertsWrapper } from '@/components/obs/ObsAlertsWrapper';
+
+const SlideyPlayzObsAlerts = () => (
+  <ObsAlertsWrapper streamerSlug="slidey_playz" storagePrefix="slidey_playz" />
+);
+
+export default SlideyPlayzObsAlerts;
