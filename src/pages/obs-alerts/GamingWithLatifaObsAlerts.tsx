54907dcb71c35fa70384f5cd2b72392ef6331aef
Ankit Kumar <ankitashuk20@gmail.com>
2026-03-18 05:11:14 +0530

    Create GamingWithLatifaObsAlerts.tsx

---
+++ src/pages/obs-alerts/GamingWithLatifaObsAlerts.tsx
@@ -0,0 +1,7 @@
+import { ObsAlertsWrapper } from '@/components/obs/ObsAlertsWrapper';
+
+const GamingWithLatifaObsAlerts = () => (
+  <ObsAlertsWrapper streamerSlug="gaming_with_latifa" storagePrefix="gaming_with_latifa" />
+);
+
+export default GamingWithLatifaObsAlerts;
