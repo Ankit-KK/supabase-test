24a5a4b2fed8683d78fa5dcb309d260555faf645
Ankit Kumar <ankitashuk20@gmail.com>
2026-03-18 05:12:43 +0530

    Create GamingWithLatifaMediaSourcePlayer.tsx

---
+++ src/pages/audio-player/GamingWithLatifaMediaSourcePlayer.tsx
@@ -0,0 +1,13 @@
+import MediaSourcePlayer from '@/components/audio-player/MediaSourcePlayer';
+
+const GamingWithLatifaMediaSourcePlayer = () => (
+  <MediaSourcePlayer
+    streamerSlug="gaming_with_latifa"
+    streamerName="Gaming With Latifa"
+    tableName="gaming_with_latifa_donations"
+    browserSourcePath="/gaming_with_latifa/media-audio-player"
+    brandColor="#a855f7"
+  />
+);
+
+export default GamingWithLatifaMediaSourcePlayer;
