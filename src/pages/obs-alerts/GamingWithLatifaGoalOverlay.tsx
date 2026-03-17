a9b2affd97936814b805244048402ec57d391f42
Ankit Kumar <ankitashuk20@gmail.com>
2026-03-18 05:12:05 +0530

    Create GamingWithLatifaGoalOverlay.tsx

---
+++ src/pages/obs-alerts/GamingWithLatifaGoalOverlay.tsx
@@ -0,0 +1,9 @@
+import { GoalOverlayWrapper } from '@/components/obs/GoalOverlayWrapper';
+
+const GamingWithLatifaGoalOverlay = () => (
+  <div className="w-screen h-screen bg-transparent overflow-hidden flex items-center justify-center">
+    <GoalOverlayWrapper streamerSlug="gaming_with_latifa" />
+  </div>
+);
+
+export default GamingWithLatifaGoalOverlay;
